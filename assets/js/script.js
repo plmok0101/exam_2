b65600531cbdea959e48e48bb8cbb73d43bed761
plmok0101 <plmokn1q2w3e@gmail.com>
2022-11-10 17:08:31 +0900

    데이터받기

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,24 +9,44 @@ function getPuuid(){
 function getTotalID(){
     $.getJSON("https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=10&api_key=" + api_key, function(data){
         game = (data);
-        alert(game);
-    })
+    })    
 }
+
+//게임정보 가져오기
+function getGame(){
+    for(let i = 0; i < 10; i++){
+        $.getJSON("https://asia.api.riotgames.com/lol/match/v5/matches/" + game[i] + "?api_key=" + api_key,function(data){
+            for(let j = 0; j <10; j++){
+                $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text((data.info.participants[j].championName));//챔피언
+                $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text((data.info.participants[j].summonerName));//닉
+                $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text((data.info.participants[j].kills));//킬
+                $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text((data.info.participants[j].deaths));//데스
+                $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text((data.info.participants[j].assists));//어시
+                $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text((data.info.participants[j].totalMinionsKilled));//수정필요
+                $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text((data.info.participants[j].totalDamageDealtToChampions));//딜량
+                $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text((data.info.participants[j].goldEarned));//획득골드
+            }
+        })
+    }
+
+}
+//div생성
 function createDiv(){
     for (let i = 0; i < 10; i++){
         $(".game"+[i]).after("<div class=\"game"+[i+1]+"\"></div>");
         for(let j = 0; j <10; j++){
-            $(".game"+[i+1]).append("<div class=\"pick"+[j+1]+"\"></div>"); 
+            $(".game"+[i+1]).append("<div class=\"pick"+[j+1]+"\"></div>");  
+        }
+    }
+    for(let i = 0; i<10; i++){
+        for(let n = 0; n <8; n++){
+            $(".pick"+[i+1]).append("<span class=\"span"+[n+1]+"\"></span>");
         }
     }
 }
 
 $(document).ready(function(){
-
-    $("#btn").click(function(){
-        createDiv();
-    })
-    
+    createDiv(); 
     let nickname;
     let game = [];
     let puuid;
@@ -37,28 +57,25 @@ $(document).ready(function(){
 
         $.getJSON("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + nickname + "?api_key=" + api_key, function(data){
             puuid = (data.puuid);
-        })
-
-        $.getJSON("https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=10&api_key=" + api_key, function(data){
-            game = (data);
-        })
-
-        for(let i = 0; i < 10; i++){
-            $.getJSON("https://asia.api.riotgames.com/lol/match/v5/matches/" + game[i] + "?api_key=" + api_key,function(data){
-                for(let j = 0; j <10; j++){
-                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].championName)+"</span>");//챔피언
-                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].summonerName)+"</span>");//닉
-                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].kills)+"</span>");//킬
-                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].deaths)+"</span>");//데스
-                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].assists)+"</span>");//어시
-                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].totalMinionsKilled)+"</span>");//수정필요
-                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].totalDamageDealtToChampions)+"</span>");//딜량
-                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].goldEarned)+"</span>");//획득골드
+            $.getJSON("https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=10&api_key=" + api_key, function(data){
+                game = (data);
+                for(let i = 0; i < 10; i++){
+                    $.getJSON("https://asia.api.riotgames.com/lol/match/v5/matches/" + game[i] + "?api_key=" + api_key,function(data){
+                        for(let j = 0; j <10; j++){
+                            let n=1;
+                            $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text("  "+(data.info.participants[j].championName));n++;//챔피언
+                            $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text("  "+(data.info.participants[j].summonerName));n++;//닉
+                            $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text("  "+(data.info.participants[j].kills));n++;//킬
+                            $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text("  "+(data.info.participants[j].deaths));n++;//데스
+                            $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text("  "+(data.info.participants[j].assists));n++;//어시
+                            $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text("  "+(data.info.participants[j].totalMinionsKilled));n++;//수정필요
+                            $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text("  "+(data.info.participants[j].totalDamageDealtToChampions));n++;//딜량
+                            $(".game" + [i+1] + " > .pick"+[j+1]+ " > .span" + n).text("  "+(data.info.participants[j].goldEarned));n++;//획득골드
+                        }
+                    })
                 }
             })
-        }
-
-
+        })
     })
 })
 
