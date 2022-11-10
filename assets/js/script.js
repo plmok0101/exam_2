b994d6e11519594cc86a348589dd73af99c3b4f1
plmok0101 <plmokn1q2w3e@gmail.com>
2022-11-10 14:37:35 +0900

    데어터받기

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,40 +12,52 @@ function getTotalID(){
         alert(game);
     })
 }
+function createDiv(){
+    for (let i = 0; i < 10; i++){
+        $(".game"+[i]).after("<div class=\"game"+[i+1]+"\"></div>");
+        for(let j = 0; j <10; j++){
+            $(".game"+[i+1]).append("<div class=\"pick"+[j+1]+"\"></div>"); 
+        }
+    }
+}
 
 $(document).ready(function(){
+
+    $("#btn").click(function(){
+        createDiv();
+    })
     
     let nickname;
     let game = [];
+    let puuid;
 
     $("#form").submit(function(event){
         event.preventDefault();
         nickname = $("#nickname").val();
-        
+
         $.getJSON("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + nickname + "?api_key=" + api_key, function(data){
             puuid = (data.puuid);
         })
 
         $.getJSON("https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=10&api_key=" + api_key, function(data){
             game = (data);
-            alert(game[0]);
         })
 
-        $.getJSON("https://asia.api.riotgames.com/lol/match/v5/matches/" + game[0] + "?api_key=" + api_key,function(data){
-            $('body').append("<p>어시="+(data.info.participants[0].assists)+"</p>");
-            $('body').append("<p>킬="+(data.info.participants[0].kills)+"</p>");
-            $('body').append("<p>챔피언="+(data.info.participants[0].championName)+"</p>");
-            $('body').append("<p>데스="+(data.info.participants[0].deaths)+"</p>");
-            $('body').append("<p>획득한골드="+(data.info.participants[0].goldEarned)+"</p>");
-            $('body').append("<p>승패="+(data.info.participants[0].win)+"</p>");
-            $('body').append("<p>cs="+(data.info.participants[0].totalMinionsKilled)+"</p>");
-            $('body').append("<p>유저닉="+(data.info.participants[0].summonerName)+"</p>");
-            $('body').append("<p>딜량="+(data.info.participants[0].totalDamageDealtToChampions)+"</p>");
-            
-            //반복해서 10명데이터 모두 추출
-            
+        for(let i = 0; i < 10; i++){
+            $.getJSON("https://asia.api.riotgames.com/lol/match/v5/matches/" + game[i] + "?api_key=" + api_key,function(data){
+                for(let j = 0; j <10; j++){
+                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].championName)+"</span>");
+                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].summonerName)+"</span>");
+                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].kills)+"</span>");
+                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].deaths)+"</span>");
+                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].assists)+"</span>");
+                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].totalMinionsKilled)+"</span>");//수정필요함
+                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].totalDamageDealtToChampions)+"</span>");
+                    $(".game" + [i+1] + " > .pick"+[j+1]).append("<span>  "+(data.info.participants[j].goldEarned)+"</span>");
+                }
+            })
+        }
 
-        })
 
     })
 })
