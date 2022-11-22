7850d7d9239a4e2eed6834726bb17d6213e00dbd
plmok0101 <plmokn1q2w3e@gmail.com>
2022-11-22 17:08:16 +0900

    ajax 동기

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,133 @@ let btMs;
 let btDay;
 let teamId;
 
+function ajax3(i){
+    html = document.querySelector("#template").innerHTML;
+    html2 = document.querySelector("#template2").innerHTML;
+    $.ajax({
+        type: "get",
+        url:`https://asia.api.riotgames.com/lol/match/v5/matches/${game[i]}?api_key=${api_key}`,
+        dataType:"json",
+        async: false,
+        success: function(data){
+            team1 ="";
+            team2 = "";
+    
+            min = parseInt((data.info.gameDuration)/60);
+            sec = parseInt((data.info.gameDuration)%60);
+    
+            wlBlue = getWl(data.info.participants[0].win);
+            wlRed = getWl(data.info.participants[5].win); 
+            gamemode = getgamemode((data.info.queueId));
+    
+            date = new Date(data.info.gameStartTimestamp);
+            btDay = date2(data.info.gameStartTimestamp);
+    
+            date = `${date.getFullYear()}년${date.getMonth()+1}월${date.getDate()}일`;
+            if(btDay<10){
+                $(".con").append(
+                    html.replace(`id ="game"`, `id ="game${i+1}"`)
+                         .replace(`{time}`,`${min}:${sec}`)
+                         .replace(`{gamemode}`,gamemode)
+                         .replace(`{date}`,btDay+"일전")
+                         .replace(`{Wl}`,)
+                         .replace(`id ="teamPlusBtn"`,`id ="teamPlusBtn${i+1}"`)
+                );
+            }else{
+                $(".con").append(
+                    html.replace(`id ="game"`, `id ="game${i+1}"`)
+                         .replace(`{time}`,`${min}:${sec}`)
+                         .replace(`{gamemode}`,gamemode)
+                         .replace(`{date}`,date)
+                         .replace(`class ="teamPlusBtn"`,`class ="teamPlusBtn${i+1}"`)
+                );
+            }
+            for(let n = 0; n<10; n++){
+                if(nickname == (data.info.participants[n].summonerName).replace(/(\s*)/g, "").toUpperCase()){
+                    teamId = (data.info.participants[n].teamId);
+                    teamOne = getTeam(teamId);
+                    teamTwo = getTeam2(teamId);
+                    if(data.info.participants[n].win){
+                        $(`#game${i+1} > .head > #WL`).text("승리");
+                        $(`#game${i+1} > .div2 > .team1 > .flex >.wl`).text(`승리(${teamOne})`);
+                        $(`#game${i+1} > .div2 > .team2 > .flex >.wl`).text(`패배(${teamTwo})`);
+                        $(`#game${i+1} > .head > #WL`).addClass(`winColor`);
+                        $(`#game${i+1} > .div2 > .team1`).addClass(`WIN`);
+                        $(`#game${i+1} > .div2 > .team2`).addClass(`LOSE`);
+                        $(`#game${i+1} > .head`).addClass(`WIN`);
+                    }else{
+                        $(`#game${i+1} > .head > #WL`).text("패배");
+                        $(`#game${i+1} > .div2 > .team1 > .flex >.wl`).text(`패배(${teamOne})`);
+                        $(`#game${i+1} > .div2 > .team2 > .flex >.wl`).text(`승리(${teamTwo})`);
+                        $(`#game${i+1} > .head > #WL`).addClass(`loseColor`);
+                        $(`#game${i+1} > .div2 > .team1`).addClass(`LOSE`);
+                        $(`#game${i+1} > .div2 > .team2`).addClass(`WIN`);
+                        $(`#game${i+1} > .head`).addClass(`LOSE`);
+                    }
+                }
+                spel1 = getSpell(data.info.participants[n].summoner1Id);
+                spel2 = getSpell(data.info.participants[n].summoner2Id);
+    
+                item1 = getitem(data.info.participants[n].item0);
+                item2 = getitem(data.info.participants[n].item1);
+                item3 = getitem(data.info.participants[n].item2);
+                item4 = getitem(data.info.participants[n].item3);
+                item5 = getitem(data.info.participants[n].item4);
+                item6 = getitem(data.info.participants[n].item5);
+                ward = getitem(data.info.participants[n].item6);
+    
+                if(n<5){
+                    team1 += html2.replace(`{champion}`,`<img src="https://opgg-static.akamaized.net/meta/images/lol/champion/${data.info.participants[n].championName}.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto,f_webp,w_92&v=1668492741460" class="size"/>`)
+                                       .replace(`{champLevel}`,data.info.participants[n].champLevel)
+                                       .replace(`{spell1}`,`<img src="https://opgg-static.akamaized.net/meta/images/lol/spell/Summoner${spel1}.png?image=q_auto,f_webp,w_auto&v=1668492741460" class="size"/>`)
+                                       .replace(`{spell2}`,`<img src="https://opgg-static.akamaized.net/meta/images/lol/spell/Summoner${spel2}.png?image=q_auto,f_webp,w_auto&v=1668492741460" class="size"/>`)
+                                       .replace(`{userName}`,data.info.participants[n].summonerName)
+                                       .replace(`{item1}`,item1)
+                                       .replace(`{item2}`,item2)
+                                       .replace(`{item3}`,item3)
+                                       .replace(`{item4}`,item4)
+                                       .replace(`{item5}`,item5)
+                                       .replace(`{item6}`,item6)
+                                       .replace(`{ward}`,ward)
+                                       .replace(`{k}`,data.info.participants[n].kills+"/")
+                                       .replace(`{d}`,data.info.participants[n].deaths+"/")
+                                       .replace(`{a}`,data.info.participants[n].assists)
+                                       .replace(`{cs}`,(data.info.participants[n].totalMinionsKilled) + (data.info.participants[n].neutralMinionsKilled))
+                                       .replace(`{damage}`,data.info.participants[n].totalDamageDealtToChampions)
+                                       .replace(`{gold}`,data.info.participants[n].goldEarned);
+                }else{
+                    team2 += html2.replace(`{champion}`,`<img src="https://opgg-static.akamaized.net/meta/images/lol/champion/${data.info.participants[n].championName}.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto,f_webp,w_92&v=1668492741460" class="size"/>`)
+                                      .replace(`{champLevel}`,data.info.participants[n].champLevel)
+                                      .replace(`{spell1}`,`<img src="https://opgg-static.akamaized.net/meta/images/lol/spell/Summoner${spel1}.png?image=q_auto,f_webp,w_auto&v=1668492741460" class="size"/>`)
+                                      .replace(`{spell2}`,`<img src="https://opgg-static.akamaized.net/meta/images/lol/spell/Summoner${spel2}.png?image=q_auto,f_webp,w_auto&v=1668492741460" class="size"/>`)
+                                      .replace(`{userName}`,data.info.participants[n].summonerName)
+                                      .replace(`{item1}`,item1)
+                                      .replace(`{item2}`,item2)
+                                      .replace(`{item3}`,item3)
+                                      .replace(`{item4}`,item4)
+                                      .replace(`{item5}`,item5)
+                                      .replace(`{item6}`,item6)
+                                      .replace(`{ward}`,ward)
+                                      .replace(`{k}`,data.info.participants[n].kills+"/")
+                                      .replace(`{d}`,data.info.participants[n].deaths+"/")
+                                      .replace(`{a}`,data.info.participants[n].assists)
+                                      .replace(`{cs}`,data.info.participants[n].totalMinionsKilled + (data.info.participants[n].neutralMinionsKilled))
+                                      .replace(`{damage}`,data.info.participants[n].totalDamageDealtToChampions)
+                                      .replace(`{gold}`,data.info.participants[n].goldEarned);
+               }
+            }
+            if(teamId == 100){
+                document.querySelector(`#game${i+1} .team1_2`).innerHTML = team1;
+                document.querySelector(`#game${i+1} .team2_2`).innerHTML = team2; 
+            }
+            if(teamId == 200){
+                document.querySelector(`#game${i+1} .team1_2`).innerHTML = team2;
+                document.querySelector(`#game${i+1} .team2_2`).innerHTML = team1; 
+            }
+        }
+    })
+}
+
 
 function getgamemode(data){
     switch(data){
@@ -282,8 +409,8 @@ $(document).ready(function(){
             puuid = (data.puuid);
             $.getJSON(`https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=${api_key}`, function(data){
                 game = (data);
-                for(let i = 0; i < 5; i++){
-                    getjosn3(i);
+                for(let i =0; i<5; i++){
+                    ajax3(i);
                 }
 
             })
@@ -299,7 +426,7 @@ $(document).ready(function(){
         $(".loading").css("display", 'inline-block');
         setTimeout(function(){
             for(let i = 0+a ; i < 5+a ; i++){
-                getjosn3(i);
+                ajax3(i);
             }
             $(".loading").css("display", 'none');
             $(".bot").insertAfter(".con");
@@ -316,7 +443,6 @@ $(document).ready(function(){
     })
 })
 $(document).on("click","#teamPlusBtn1",function(){
-    console.log(1234);
     $("#game1 > .div2 > .team2").toggle();
 })
 $(document).on("click","#teamPlusBtn2",function(){
